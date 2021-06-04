c990220884d3ac645ea88803f79bbf3df12c461c
Gupta <deepakgupta63@deloitte.com>
2021-06-04 17:37:36 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import {Cardlist} from './components/card-list/card-list.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {SearchBox} from './components/search-box/search-box.component'
+
+// class App extends Component{
+//   constructor(){
+//     super();
+
+//     this.state = {
+//       valueString: 'Hello Deepak'
+//     }
+//   }
+
+
+//   render(){
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//            {this.state.valueString}
+//           </p>
+//           <button onClick = {()=>this.setState({valueString: 'You Clicked the button'})}>Click to change</button>
+//         </header>
+//       </div>
+//     );
+//   }
+
+// }
+
+
+
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name:'Frankenstine',
+          id: 1
+        },
+        {
+          name:'Zombie',
+          id:2
+        },
+        {
+          name:'Dracula',
+          id:3
+        },
+      ],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then(users => {
+      console.log(users);
+      this.setState({monsters: users})
+    })
+  }
+
+
+  render(){
+
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+    return (
+      <div className="App">
+        {
+        /* <input type = 'search'
+         placeholder='Search Monster'
+         onChange = {e => this.setState({searchField: e.target.value}, () => console.log(this.state.searchField))} /> */}
+         <h1 className = 'title'>Monster Finder</h1>
+        <SearchBox placeholder='Search Monster' handleChange = {e => this.setState({searchField: e.target.value})}/>
+        <Cardlist monsters = {filteredMonsters}> 
+         </Cardlist>
+      </div>
+    );
+  }
+
 }
+
+// function App() {
+  
+// }
 
 export default App;
